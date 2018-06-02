301b032c0c129a05623e8ef6d7b732b6d0e17fbf
James Meehan <ghoyas2003@gmail.com>
2018-06-01 22:49:37 -0400

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'us-cdbr-iron-east-04.cleardb.net',
-  user            : 'bef0bc73b59560',
-  password        : 'b7cc499b',
-  database        : 'heroku_fa00f92c3667a35',
+  host            : [your host],
+  user            : [your username],
+  password        : [your password],
+  database        : [your database],
   dateStrings	  : true
 });
 
